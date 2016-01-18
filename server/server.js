bfa60569bf1ea788a3400034645e65d35a490851
JeremyCloutier <jeremycloutier@Jeremys-MacBook-Pro.local>
2016-01-17 23:27:48 -0600

    First commit, contains basic server and project structure.

---
+++ server/server.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jeremycloutier on 1/17/16.
+ */
+var express = require('express');
+var app = express();
+
+app.use(express.static('server/public'));
+
+app.get('/', function(request, response){
+    response.sendFile(__dirname + '/public/views/index.html');
+});
+
+var server = app.listen(3000, function(){
+    var port = server.address().port;
+    console.log('Listening on port', port);
+});
